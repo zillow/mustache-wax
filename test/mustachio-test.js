94a59b50556c246e7bc9a5bedf72f6aef56ab816
Daniel Stockman <daniels@zillow.com>
2012-07-03 11:46:26 -0700

    Ensure modulePrefix has a default, and test instantiation with no config.

--- test/mustachio-test.js
+++ test/mustachio-test.js
@@ -18,6 +18,21 @@ var fixtures = {
 };
 
 module.exports = {
+    "should instantiate with defaults when no config passed": function (test) {
+        test.expect(5);
+
+        var instance = new Mustachio();
+
+        test.ok(Array.isArray(instance.output), "Instance 'output' property not set.");
+        test.ok('object' === typeof instance.known, "Instance 'known' property not set.");
+
+        test.strictEqual(instance.argv.modulePrefix, "template", "Incorrect modulePrefix default.");
+        test.strictEqual(instance.prefix, "template-", "Incorrect instance.prefix default.");
+
+        test.ok(instance.pregex.test('"template-foo"'), "Prefix regular expression incorrect.");
+
+        test.done();
+    },
     "should generate simple output": function (test) {
         test.expect(2);
 
@@ -26,8 +41,8 @@ module.exports = {
         });
 
         instance.invoke([__dirname + '/templates/alpha.mustache']);
-        test.ok(instance.output.length === 1, "Output array empty");
 
+        test.strictEqual(instance.output.length, 1, "Output array empty");
         test.strictEqual(instance.render(), fixtures.emptyCompiledFunction, "Simple output mismatch");
 
         test.done();
