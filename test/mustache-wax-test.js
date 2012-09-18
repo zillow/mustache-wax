ecda03649db6eaf23146a6149583b85310d6b308
Daniel Stockman <daniels@zillow.com>
2012-09-18 16:12:14 -0700

    Need to transition to hogan, since it's actually actively maintained and compliant with the mustache spec...

--- test/mustache-wax-test.js
+++ test/mustache-wax-test.js
@@ -17,6 +17,84 @@ if (existsSync(testOutputDir)) {
 
 var fixtures = {
 
+    deeplyNestedContext: {
+        "a": { "one": 1 },
+        "b": { "two": 2 },
+        "c": { "three": 3 },
+        "d": { "four": 4 },
+        "e": { "five": 5 }
+    },
+
+    deeplyNestedHTML: [
+        '1',
+        '121',
+        '12321',
+        '1234321',
+        '123454321',
+        '1234321',
+        '12321',
+        '121',
+        '1',
+        ''
+    ].join('\n'),
+
+    scopeBubbledContext: {
+        "grades": [
+            {
+                "grade_level": "3",
+                "years": ['1999', '2001']
+            },
+            {
+                "grade_level": "5",
+                "years": ['2003','2012']
+            }
+        ]
+    },
+
+    scopeBubbledHTML: [
+        '    <ul id="grade-3">',
+        '        <li class="year">1999</li>',
+        '        <li class="year">2001</li>',
+        '    </ul>',
+        '    <ul id="grade-5">',
+        '        <li class="year">2003</li>',
+        '        <li class="year">2012</li>',
+        '    </ul>',
+        ''
+    ].join('\n'),
+
+    scopedBubbleCompiled: [
+        'function (Handlebars,depth0,helpers,partials,data) {',
+        '  helpers = helpers || Handlebars.helpers;',
+        '  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;',
+        '',
+        'function program1(depth0,data) {',
+        '  ',
+        '  var buffer = "", stack1;',
+        '  buffer += "\\n    ";',
+        '  stack1 = depth0.years;',
+        '  stack1 = helpers.each.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(2, program2, data)});',
+        '  if(stack1 || stack1 === 0) { buffer += stack1; }',
+        '  buffer += "\\n";',
+        '  return buffer;}',
+        'function program2(depth0,data) {',
+        '  ',
+        '  var buffer = "", stack1, foundHelper;',
+        '  buffer += "\\n    <figure id=\\"fig-";',
+        '  foundHelper = helpers.grade_level;',
+        '  if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{}}); }',
+        '  else { stack1 = depth0.grade_level; stack1 = typeof stack1 === functionType ? stack1() : stack1; }',
+        '  buffer += escapeExpression(stack1) + "\\" />\\n    ";',
+        '  return buffer;}',
+        '',
+        '  stack1 = depth0.grades;',
+        '  stack1 = helpers.each.call(depth0, stack1, {hash:{},inverse:self.noop,fn:self.program(1, program1, data)});',
+        '  if(stack1 || stack1 === 0) { buffer += stack1; }',
+        '  buffer += "\\n";',
+        '  return buffer;}',
+        ''
+    ].join('\n'),
+
     emptyCompiledFunction: [
         'function (Handlebars,depth0,helpers,partials,data) {',
         '  helpers = helpers || Handlebars.helpers;',
@@ -210,3 +288,72 @@ exports.rendering = {
     }
 
 }; // end rendering tests
+
+exports.templating = {
+
+    "hogan should compile and render deeply nested context correctly": function (test) {
+        test.expect(3);
+
+        var filePath = path.join(__dirname, 'syntax', 'deeply-nested.mustache');
+
+        fs.readFile(filePath, 'utf8', function (err, mustache) {
+            test.ifError(err);
+
+            var hogan = require("hogan.js");
+            var template = hogan.compile(mustache);
+            test.ok('function' === typeof template.render, 'template compilation failed');
+
+            var actual = template.render(fixtures.deeplyNestedContext);
+            test.strictEqual(actual, fixtures.deeplyNestedHTML, 'deeply nested html mismatch');
+            // console.log(actual);
+
+            test.done();
+        });
+    },
+
+/*
+    "should generate simple scope-bubbled correctly": function (test) {
+        test.expect(3);
+
+        var filePath = path.join(__dirname, 'output', 'scope-bubbled.js');
+        var instance = new MustacheWax({
+            simple: true,
+            outputFile: filePath
+        });
+
+        instance.invoke(__dirname + '/syntax/scope-bubbled.mustache');
+
+        instance.renderOutput(function (err) {
+            test.ifError(err);
+
+            fs.readFile(filePath, 'utf8', function (foul, actual) {
+                test.ifError(foul);
+
+                test.strictEqual(actual, fixtures.scopedBubbleCompiled, "Scope bubbled output mismatch.");
+
+                test.done();
+            });
+        });
+    },
+*/
+    "hogan should revive scope-bubbled template correctly": function (test) {
+        test.expect(3);
+
+        var filePath = path.join(__dirname, 'syntax', 'scope-bubbled.mustache');
+
+        fs.readFile(filePath, 'utf8', function (err, mustache) {
+            test.ifError(err);
+
+            var hogan = require("hogan.js");
+            var template = hogan.compile(mustache);
+            test.ok('function' === typeof template.render, 'template compilation failed');
+
+            var actual = template.render(fixtures.scopeBubbledContext);
+            console.log(actual);
+            test.strictEqual(actual, fixtures.scopeBubbledHTML, 'scope bubbled html mismatch');
+
+            test.done();
+        });
+    }
+
+}; // end templating tests
